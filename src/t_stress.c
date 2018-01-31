0d5fde25d4dc0c52c79fb3cb76cee343c0893b77
Alexander Kiselev <kiselev99@gmail.com>
2018-02-01 02:13:45 +0300

    add qsbr stress test

--- src/t_stress.c
+++ src/t_stress.c
@@ -16,6 +16,7 @@
 #include <err.h>
 
 #include "ebr.h"
+#include "qsbr.h"
 #include "utils.h"
 
 static unsigned			nsec = 10; /* seconds */
@@ -40,6 +41,7 @@ typedef struct {
 
 static unsigned			magic_val = MAGIC_VAL;
 static ebr_t *			ebr;
+static qsbr_t *		qsbr;
 static data_struct_t		ds[DS_COUNT]
     __attribute__((__aligned__(CACHE_LINE_SIZE)));
 
@@ -128,6 +130,101 @@ ebr_stress(void *arg)
 	return NULL;
 }
 
+/*
+ * Start new epoch and wait until all registered threads have observed it
+ */
+static void
+wait(const struct timespec sleep)
+{
+   qsbr_epoch_t new_epoch = qsbr_barrier(qsbr);
+   while (!qsbr_sync(qsbr, new_epoch))
+      (void)nanosleep(&sleep, NULL);
+}
+
+static void
+qsbr_writer(unsigned target)
+{
+	data_struct_t *obj = &ds[target];
+
+	if (obj->visible) {
+		/*
+		 * The data structure is visible.  First, ensure it is no
+		 * longer visible (think of "remove" semantics).
+		 */
+		assert(obj->visible);
+		obj->visible = false;
+
+		/* make sure all readers have stopped using target object */
+		struct timespec sleep;
+		sleep.tv_sec = 0;
+		sleep.tv_nsec = 100;
+		wait(sleep);
+
+		/* now it's safe to modify the target */
+		obj->ptr = NULL;
+	}
+	else {
+		/* object is not visible.
+		 * publish it back.
+		 */
+		obj->ptr = &magic_val;
+		atomic_thread_fence(memory_order_release);
+		assert(!obj->visible);
+		obj->visible = true;
+	}
+}
+
+static void *
+qsbr_stress(void *arg)
+{
+	const unsigned id = (uintptr_t)arg;
+	unsigned n = 0;
+
+	qsbr_register(qsbr);
+
+	/*
+	 * There are NCPU threads concurrently reading data and a single
+	 * writer thread (ID 0) modifying data.  The writer will modify
+	 * the pointer used by the readers to NULL as soon as it considers
+	 * the object ready for reclaim.
+	 */
+
+	pthread_barrier_wait(&barrier);
+	while (!stop) {
+		n = ++n & (DS_COUNT - 1);
+
+		if (id == 0) {
+			qsbr_writer(n);
+			continue;
+		}
+
+		/*
+		 * Reader: iterate through the data structures and,
+		 * if the object is visible (think of "lookup" semantics),
+		 * read its value through a pointer.  The writer will set
+		 * the pointer to NULL when it thinks the object is ready
+		 * to be reclaimed.
+		 *
+		 * Incorrect reclamation mechanism would lead to the crash
+		 * in the following pointer dereference.
+		 */
+
+		if (ds[n].visible && *ds[n].ptr != MAGIC_VAL)
+			abort();
+
+		qsbr_checkpoint(qsbr);
+	}
+
+	/* ensure that the writer will stop
+	 * if it waits for readers
+	 */
+	qsbr_checkpoint(qsbr);
+
+	pthread_barrier_wait(&barrier);
+	pthread_exit(NULL);
+	return NULL;
+}
+
 static void
 ding(int sig)
 {
@@ -159,7 +256,6 @@ run_test(void *func(void *))
 	 * Create some data structures and the EBR object.
 	 */
 	memset(&ds, 0, sizeof(ds));
-	ebr = ebr_create();
 
 	/*
 	 * Spin the test.
@@ -176,8 +272,10 @@ run_test(void *func(void *))
 		pthread_join(thr[i], NULL);
 	}
 	pthread_barrier_destroy(&barrier);
-	ebr_destroy(ebr);
 }
+
+#define RA_QSBR 1
+#define RA_EBR 2
 
 int
 main(int argc, char **argv)
@@ -185,8 +283,27 @@ main(int argc, char **argv)
 	if (argc >= 2) {
 		nsec = (unsigned)atoi(argv[1]);
 	}
-	puts("stress test");
-	run_test(ebr_stress);
+
+	int recl_alg = RA_EBR;
+	if (argc >= 3 && strcmp(argv[2], "qsbr") == 0)
+		recl_alg = RA_QSBR;
+
+	switch (recl_alg) {
+		case RA_QSBR:
+			puts("QSBR stress test");
+			qsbr = qsbr_create();
+			run_test(qsbr_stress);
+			qsbr_destroy(qsbr);
+			break;
+
+		case RA_EBR:
+			puts("EBR stress test");
+			ebr = ebr_create();
+			run_test(ebr_stress);
+			ebr_destroy(ebr);
+			break;
+	}
+
 	puts("ok");
 	return 0;
 }
